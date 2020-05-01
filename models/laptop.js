ce654491f3ef0ce680932eeb7ed8e3e0d73d3ae1
KJTempas <fw6927mm@go.minneapolis.edu>
2020-04-30 21:11:25 -0500

    thru slide 24; dbase and tables created

---
+++ models/laptop.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Laptop = sequelize.define('Laptop', {
+    brand: DataTypes.STRING,
+    model: DataTypes.STRING,
+    serialNumber: DataTypes.STRING,
+    unique: true
+  }, {});
+  Laptop.associate = function(models) {
+    // associations can be defined here
+    Laptop.belongsTo(models.Employee, {
+      foreignKey: 'employeeId', 
+      onDelete: 'SET NULL'
+    })
+  };
+  return Laptop;
+};
